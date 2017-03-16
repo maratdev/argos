df1f03acbdc8ae96a3516245d1b611fc0b8c420f
Марат Закарьяев < >
2017-03-16 19:23:36 +0300

    Добавление адреса

--- app/js/common.js
+++ app/js/common.js
@@ -1,20 +1,18 @@
-$(function(){
+$(document).ready(function(){
 
     //Header во весь экран
     function heightDetect(){
-        $(".max-height").css("height", $(window).height());
-
+        $(".js-max-height").css("height", $(window).height());
     }
-
     heightDetect();
     $(window).resize(function() {
         heightDetect();
     });
 
+    // Хедер--> Видео
     var hasMask = document.implementation.hasFeature("http://www.w3.org/TR/SVG11/feature#Mask", "1.0");
-
     if (!hasMask) {
-        document.querySelector('.svg-header').display = 'none';
-        document.querySelector('.no-svg-header').display = 'block';
+        document.querySelector('.header__svg_header').display = 'none';
+        document.querySelector('.js-no-svg-header').display = 'block';
     }
 });
