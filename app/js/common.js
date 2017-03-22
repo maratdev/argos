c1d0a6500d004161ab6d1be360517a9018d1ecaa
Марат Закарьяев < >
2017-03-22 12:14:52 +0300

    Добавление фото в порфтолио

--- app/js/common.js
+++ app/js/common.js
@@ -2,6 +2,15 @@ $(document).ready(function(){
 
     $('.js-focuspoint').focusPoint();
 
+
+    $('.grid').masonry({
+        itemSelector: '.grid-item',
+        columnWidth: 160,
+        isFitWidth: true,
+        gutter: 5,
+        transitionDuration: '0.8s'
+    });
+
     //Header во весь экран
     function heightDetect(){
         $(".js-max-height").css("height", $(window).height());
@@ -11,6 +20,7 @@ $(document).ready(function(){
         heightDetect();
     });
 
+
     // Хедер--> Видео
     var hasMask = document.implementation.hasFeature("http://www.w3.org/TR/SVG11/feature#Mask", "1.0");
     if (!hasMask) {
@@ -18,19 +28,42 @@ $(document).ready(function(){
         document.querySelector('.js-no-svg-header').display = 'block';
     }
 
-// external js: masonry.pkgd.js, imagesloaded.pkgd.js
+    $(".hover").mouseleave(
+        function () {
+            $(this).removeClass("hover");
+        }
+    );
 
-// init Masonry
-    var $grid = $('.grid').masonry({
-        itemSelector: '.grid-item',
-        percentPosition: true,
-        columnWidth: '.grid-sizer'
+
+
+});
+
+    $(".element-in").slice(0, 4).show();
+    $("#loadMore").on('click', function (e) {
+        e.preventDefault();
+        $("div:hidden").slice(0, 4).slideDown();
+        if ($("div:hidden").length == 0) {
+            $("#load").fadeOut('slow');
+        }
+        $('html,body').animate({
+            scrollTop: $(this).offset().top
+        }, 2000);
     });
 
 
+$('a[href=#top]').click(function () {
+    $('body,html').animate({
+        scrollTop: 0
+    }, 1200);
+    return false;
+});
 
-
-
+$(window).scroll(function () {
+    if ($(this).scrollTop() > 600) {
+        $('.totop a').fadeIn();
+    } else {
+        $('.totop a').fadeOut();
+    }
 });
 
 
--- gulpfile.js
+++ gulpfile.js
@@ -19,6 +19,7 @@ gulp.task('scripts', function() {
 	return gulp.src([
 		'app/libs/jquery/dist/jquery.min.js',
 		'app/libs/detected/detect.js',
+		'app/libs/grid/mansory-gr.js',
 		'app/libs/focuspoint/jquery.focuspoint.min.js',
 		'app/js/common.js',// Всегда в конце
 		])
